b5f03bf6d6b8dae8e13fab729698b40ce8ba7a85
OS-SiGNas <signas13@gmail.com>
2022-06-03 03:02:23 -0400

    tercer commit

--- correct_answer/urls.py
+++ correct_answer/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from quizs.views import menu_quizs, resultados, render_quizs
+from quizs.views import menu_quizs, results, render_quizs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', menu_quizs),
     path('quiz/<int:id>', render_quizs),
-    path('resultados/<int:id>', resultados)
+    path('resultados/<int:id>', results)
 ]
--- quizs/views.py
+++ quizs/views.py
@@ -12,26 +12,34 @@ def render_quizs(request,id):
         "questionsTrueFalse":True_False.objects.filter(quiz_id=id)
     })
 
-def resultados(request,id):
+def results(request,id):
+    # creando lista respuestas correctas traidas de la base de datos
     questions_multiselect = Multiple.objects.filter(quiz_id=id)
     questions_true_false =  True_False.objects.filter(quiz_id=id)
-    listCorrectDB = []
+    listCorrectDB=[]
     for i in questions_multiselect:
         listCorrectDB.append(str(i.correct))
     for i in questions_true_false:
         listCorrectDB.append(str(i.correct))
-    total = len(listCorrectDB)
-    puntaje = 0
-    resul = request.POST
-    for i in listCorrectDB:
-        if i in resul.values():
-            puntaje += 1
+    print("s --->", listCorrectDB)
 
-    # respuesta = "Tu puntaje es: %d / %d"%(puntaje,total)
-    print(f"Puntaje: {puntaje} de {total}")
-    print("---------------------------------------------")
+    # Creando lista de respuestas recibidas por http
+    resul_list = list(request.POST.values())
+    resul_list.pop(0)
+    print("r --->", resul_list)
+
+    total = len(listCorrectDB) # Cantidad de preguntas en el Quiz 
+    scores = 0
+
+    # Comparando Respuestas correctas en ambas listas
+    for i in range(total):
+        if listCorrectDB[i] == resul_list[i]:
+            scores += 1
+
+    print(f" ====== Puntaje: {scores} de {total} ====== ")
+
     return render(request, 'resultado.html', {
-        "puntaje":puntaje,
+        "scores":scores,
         "total":total,
         "n":id
     })
